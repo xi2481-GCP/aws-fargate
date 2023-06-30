59c0db93b7d60efca9b018fd7d178e3eb9b00a29
xi2481-santosh <santosh.nagargoje@xebia.com>
2023-06-30 18:59:25 +0530

    update name

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  container_name = "Testing-fargate"
+  container_name = "ecsdemo-frontend"
   container_port = 80
 
   tags = {
